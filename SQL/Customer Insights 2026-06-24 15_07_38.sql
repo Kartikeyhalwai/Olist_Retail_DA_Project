4fbc41572ddde3f3f5c1de6a5f2076f83f328dd4
Kartikeyhalwai <halwaikartikeya@outlook.com>
2026-07-01 08:50:18 +0000

    CREATED SQL QUERY FILES FOR CUSTOMER INSIGHTS, CUSTOMER DISTRIBUTION AND MONTHLY REVENUE TRENDS PRODUCT SALES ANALYSIS AND RANKING

---
+++ SQL/Customer Insights 2026-06-24 15_07_38.sql
@@ -0,0 +1,16 @@
+USE CATALOG olist_retail;
+USE SCHEMA gold_dataset;
+CREATE OR REPLACE TABLE GOLD_DATASET.customer_behavior AS
+
+SELECT
+    customer_unique_id,
+    COUNT(DISTINCT order_id) AS total_orders,
+
+    CASE
+        WHEN COUNT(DISTINCT order_id) = 1 THEN 'New'
+        WHEN COUNT(DISTINCT order_id) > 1 THEN 'Repeat'
+        ELSE 'Unknown'
+    END AS customer_type
+
+FROM GOLD_DATASET.fact_sales
+GROUP BY customer_unique_id;
